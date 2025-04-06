9bf67b9dd7ec815d784396a2519faee55a62054b
Mohd Irfan <mohdirfan70097@gmail.com>
2025-04-06 20:41:00 +0530

    Printing 100 times helo world

---
+++ loopss.java
@@ -0,0 +1,14 @@
+public class loopss {
+    public static void main(String[] args) {
+
+        // printing hello word 100 times
+        String name = "Hello World";
+        int i=1;
+        while (i<100) {
+            System.out.println(name);
+          i++;
+        }
+
+    }
+
+}
